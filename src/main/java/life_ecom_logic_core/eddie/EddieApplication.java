8ea95ae1052bc0efdf7230c68e980ba35a6018a2
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2025-10-21 01:25:08 -0500

    first commit

---
+++ src/main/java/life_ecom_logic_core/eddie/EddieApplication.java
@@ -0,0 +1,13 @@
+package life_ecom_logic_core.eddie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EddieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EddieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/life_ecom_logic_core/eddie/EddieApplicationTests.java
@@ -0,0 +1,13 @@
+package life_ecom_logic_core.eddie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EddieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
